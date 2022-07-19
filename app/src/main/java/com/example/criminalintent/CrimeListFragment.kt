a416a8e33c1d2904ad3c04ea54402bfc281cdd95
Lukasz Jankowski <lukasz.jankowski1310@gmail.com>
2022-07-20 00:19:00 +0200

    Chapter 11 Databases and the Room Library

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -6,28 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.text.format.DateFormat
+import androidx.lifecycle.Observer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView: RecyclerView
-    private var adapter : CrimeAdapter? = null
+  //  private var l : List<Crime> = listOf(Crime())
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this).get(CrimeListViewModel::class.java)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,15 +39,32 @@ class CrimeListFragment : Fragment() {
     ): View? {
 
         val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
-
         crimeRecyclerView = view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        adapter = CrimeAdapter(crimeListViewModel.crimes)
         crimeRecyclerView.adapter = adapter
 
-        return view
 
+
+
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+            })
+    }
+
+    private fun updateUI(crimes: List<Crime>) {
+        adapter = CrimeAdapter(crimes)
+        crimeRecyclerView.adapter = adapter
     }
 
     private inner class CrimeAdapter(var crimes: List<Crime>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -88,7 +108,7 @@ class CrimeListFragment : Fragment() {
         private lateinit var crime : Crime
         private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
-
+        private val solvedImageView : ImageView = itemView.findViewById(R.id.crime_solved)
 
         init {
             itemView.setOnClickListener(this)
@@ -96,8 +116,17 @@ class CrimeListFragment : Fragment() {
 
         fun bind(crime : Crime) {
             this.crime = crime
+
+            val crimeDate = DateFormat.format("dd MMMM yyyy  EEEE", this.crime.date)
             titleTextView.text = this.crime.title
-            dateTextView.text = this.crime.date.toString()
+            dateTextView.text = crimeDate.toString()
+
+
+            solvedImageView.visibility = if (crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
@@ -122,7 +151,8 @@ class CrimeListFragment : Fragment() {
         fun bind(crime : Crime) {
             this.crime = crime
             titleTextView.text = this.crime.title
-            dateTextView.text = this.crime.date.toString()
+            val crimeDate = DateFormat.format("dd MMMM yyyy  EEEE", this.crime.date)
+            dateTextView.text = crimeDate.toString()
 
             reportButton.setOnClickListener {
                 Log.i("Click", "button")
